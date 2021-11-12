0a81b956b91b60c2cf507addacb7dbb3ea62beca
hugh loughrey <hloughrey@hughs-MacBook-Pro.local>
2021-11-12 11:48:21 +0000

    initial express api commit

---
+++ apps/services/catalog/src/main.ts
@@ -0,0 +1,18 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+
+const app = express();
+
+app.get('/api', (request, res) => {
+  res.send({ message: 'Welcome to catalog!' });
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
